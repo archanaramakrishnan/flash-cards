ca2a900ad41a053f8fff2bc7ab116cfd430a51fc
archanaramakrishnan <archana2359@gmail.com>
2020-05-13 12:40:48 -0700

    prints question and answer cards

--- flash_cards.hs
+++ flash_cards.hs
@@ -10,22 +10,47 @@ main = printWelcomePage >> loop
   
 loop :: IO()
 loop = do
+    args <- getArgs
+    readFromFile args
     line <- getLine
     when (line /= "Quit" && line /= "quit" && line /= "QUIT") $ do
         printFlashCard "Scientifically, diamonds need a source of light that they can reflect, illuminating their near vicinity with a radiant light of their best qualities. In the lack of favorable motivation, resources, people, and situations, this light source may disappear, but hey, you don’t ever stop being a diamond!"
         loop
 
-printWelcomePage :: IO()
-printWelcomePage = do
-    putStr "\n\n\tWelcome to the flash cards program!\n"
-    render (scale 0.8 frame) (scale 0.6 frame)
-    putStr "\n\n   Enter a lower case letter to review words: \n"
-    choice <- getChar
-    executeChoice choice
+readFromFile [] = putStr "Done in readFromFile!\n"
+readFromFile (firstArg:argsList) = do 
+                    txt <- (readFile firstArg)
+                    let questionList = grabQuestion txt
+                    let answerList = grabAnswer txt
+                    startFlashCards questionList answerList
+                    --putStr txt
+                    --readFromFile argsList
+
+startFlashCards [] [] = putStr "\nDone in startFlashCards!\n"
+startFlashCards (q:questionList) (a:answerList) = do
+                                    printFlashCard q
+                                    printFlashCard a
+                                    line <- getLine
+                                    when (line == "n") $ do
+                                        startFlashCards questionList answerList
+{-readFromFile args =  sequence_ [ do
+            txt <- readFile fileName
+            (printFlashCard ((grabQuestion txt)!!0)) | fileName <- args]-}
+
+grabQuestion :: String -> [String]
+grabQuestion txt = fmap word lst where lst = lines txt
+
+word :: String -> String
+word = head . words
+
+grabAnswer :: String -> [String]
+grabAnswer txt = fmap meaning lst where lst = lines txt
+
+meaning :: String -> String
+meaning = unwords . tail . words
 
 executeChoice :: Char -> IO()
 executeChoice txt = putChar txt
-
 
 printFlashCard :: String -> IO ()
 printFlashCard text = putStrLn $ tableString [fixedLeftCol 50, numCol] --[ColSpec]
@@ -33,20 +58,20 @@ printFlashCard text = putStrLn $ tableString [fixedLeftCol 50, numCol] --[ColSpe
                           (titlesH ["Text"]) --HeaderSpec
                           [ colsAllG center [reverseParagraph $ justifyText 50 text]] --[RowGroup a]
 
-
-
-word :: String -> String
-word = head . words
-
-meaning :: String -> String
-meaning = unwords . tail . words
-
 reverseLine :: String -> String
 reverseLine txt = unwords $ reverse $ words txt
 
 reverseParagraph :: [String] -> [String]
 reverseParagraph [] = []
 reverseParagraph (x:xs) = reverseLine x : reverseParagraph xs
+
+printWelcomePage :: IO()
+printWelcomePage = do
+    putStr "\n\n\tWelcome to the flash cards program!\n"
+    render (scale 0.8 frame) (scale 0.6 frame)
+    putStr "\n\n   Enter a lower case letter to review words: \n"
+    --choice <- getChar
+    --executeChoice choice
 
 newtype Shape = Shape (Point -> Bool)
 type Point = (Double, Double)
